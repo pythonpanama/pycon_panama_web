69e67b03caa0ab577b01961be0fc4ea17676f2d2
JorgeTerreros <jorge55terreros@gmail.com>
2025-06-27 20:17:21 -0500

    Cambios de estilos en las cartas
    Se realizaron cambios a los colores, el display y comportamientos
    responsive de las cartas de presentacion en calendar.

--- 2025/my_first_page/components/presentation.py
+++ 2025/my_first_page/components/presentation.py
@@ -1,47 +1,54 @@
-import reflex as rx
-from my_first_page.styles.styles import Size, Color
-
-
-class IterState(rx.State):
-    name: list[str] = [
-        "juan",
-        "beto",
-        "roberto",
-    ]
-
-
 # https://reflex.dev/docs/components/rendering-iterables/
+import reflex as rx
+from my_first_page.constants import EVENTS
 
 
-def name_box(name: str):
-    return rx.button(name)
-
-
-def presentation() -> rx.Component:
-    return rx.vstack(
-        rx.html(
-            """
-<link rel="stylesheet" href="/card_style.css">
-<div class="card-container">
-<div class="upper-container">
-<div class="image-container">
-    <img src="/PyConPet.jpg" alt="profile image" />
-</div>
-</div>
-
-<div class="lower-container">
-<div>
-    <h3>Mc Python</h3>
-    <h4>Front-end Developer</h4>
-</div>
-<div>
-    <p>Los efectos de la IA en la creación de páginas web.</p>
-</div>
-<div>
-    <a href="#" class="btn">View profile</a>
-</div>
-</div>
-</div>
-        """
+def presentation():
+    return rx.box(
+        # Vinculamos el CSS externo
+        rx.html('<link rel="stylesheet" href="/card_style.css">'),
+        # Estructura del div.card-container
+        rx.flex(
+            rx.foreach(
+                EVENTS,
+                lambda event: rx.box(
+                    # upper-container
+                    rx.box(
+                        rx.box(
+                            rx.image(src=event["image_url"], alt="profile image"),
+                            class_name="image-container",
+                        ),
+                        class_name="upper-container",
+                    ),
+                    # lower-container
+                    rx.box(
+                        rx.box(
+                            rx.heading(event["name"], as_="h3"),
+                            rx.heading(event["title"], as_="h4"),
+                            rx.heading(event["time"], as_="h4"),
+                        ),
+                        rx.box(rx.text(event["description"])),
+                        rx.box(
+                            rx.link(
+                                "Ver mas",
+                                href="#",
+                                class_name="btn",
+                            ),
+                        ),
+                        class_name="lower-container",
+                    ),
+                    class_name="card-container",
+                ),
+            ),
+            wrap="wrap",
+            align_items="center",
+            justify_content="center",
+            margin_x="auto",
+            gap="2rem",
+            flex_direction=["column", "column", "column", "row", "row", "row"],
+            # "repeat(auto-fit, minmax(250px, 1fr))"
+            # template_columns=[1, 2, 3],
+            # spacing="2",
+            # gap="2rem",
         ),
     )
--- 2025/my_first_page/views/calendar.py
+++ 2025/my_first_page/views/calendar.py
@@ -1,22 +1,15 @@
+from my_first_page.styles.styles import Size
 import reflex as rx
-from my_first_page.constants import EVENTS
-from my_first_page.styles.styles import Size, TextColor, Color
-import my_first_page.styles.styles as styles
 from my_first_page.components.presentation import presentation
 
 
 def calendar() -> rx.Component:
     return rx.box(
-        rx.vstack(
+        rx.hstack(
             presentation(),
-            presentation(),
-            presentation(),
+            # wrap="wrap",
         ),
-        style=styles.max_width_style,
-        margin_x="auto",
-        display="flex",
-        align_items="center",
-        justify_content="center",
         padding_y=Size.BIG.value,
-        flex_direction=["column", "column", "column", "row", "row", "row"],
+        padding_x=Size.BIG.value,
+        width="100%",
     )
