9074585176f31177fa584605dd7044ba9f291ccf
Gino Portacio <portaciogino@gmail.com>
2026-08-24 14:12:55 -0500

    fix(config): keep generated env out of repository

--- 2026/js/env.js
+++
@@ -1,7 +0,0 @@
-/**
- * PyCon Panamá 2026 - Configuración pública de cliente Supabase
- */
-window.SUPABASE_CONFIG = {
-  url: 'https://wfiyucykjoohdiazlqbz.supabase.co',
-  anonKey: 'sb_publishable_wlIN6gMmG_pVr-h-MAaLOw_jUyW4pmB'
-};
